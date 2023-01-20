c5a0f86b7687d94d1babb7d206a3dac4f16f0045
Andrea F. Daniele <afdaniele@duckietown.com>
2023-01-20 01:14:06 -0500

    added webcheck jobs

--- Makefile
+++ Makefile
@@ -3,17 +3,26 @@ ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
 ARCH:="arm32v7,arm64v8,amd64"
 
 _generate:
+	# repository jobs
 	rm -rf ${ROOT_DIR}/jobs/Docker\ Autobuild*
 	rm -rf ${ROOT_DIR}/jobs/Git\ Automerge*
 	rm -rf ${ROOT_DIR}/jobs/Stage\ Sync*
-	python3 ${ROOT_DIR}/generator/create_jobs.py \
+	python3 ${ROOT_DIR}/generator/create_repository_jobs.py \
 		--jobsdir ${ROOT_DIR}/jobs/ \
 		--repos ${ROOT_DIR}/repositories.json \
 		--arch ${ARCH} \
 		--distro ${DISTRO}
+	# webcheck jobs
+	rm -rf ${ROOT_DIR}/jobs/Webcheck*
+	python3 ${ROOT_DIR}/generator/create_webcheck_jobs.py \
+		--jobsdir ${ROOT_DIR}/jobs/ \
+		--websites ${ROOT_DIR}/websites.json \
+		--labels ${LABELS}
 
 generate-production: DISTRO=daffy,ente
+generate-production: LABELS=production
 generate-production: _generate
 
 generate-staging: DISTRO=daffy-staging,ente-staging
+generate-staging: LABELS=staging
 generate-staging: _generate
---
+++ generator/create_webcheck_jobs.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import logging
+import os
+
+logging.basicConfig()
+logger = logging.getLogger("jobs-generator")
+logger.setLevel(logging.INFO)
+
+WEBCHECK_TEMPLATE_JOB = "__webcheck_template__"
+DEFAULT_TIMEOUT_MINUTES = 1
+
+
+def main():
+    # configure arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--jobsdir",
+        required=True,
+        help="Directory containing the final jobs"
+    )
+    parser.add_argument(
+        "--websites",
+        required=True,
+        help="File containing the list of websites to check",
+    )
+    parser.add_argument(
+        "--labels",
+        required=True,
+        help="Comma-separated list of job labels to filter by",
+    )
+    parsed, _ = parser.parse_known_args()
+    # ---
+    parsed.jobsdir = os.path.abspath(parsed.jobsdir)
+    # check if the destination directory exists
+    if not os.path.exists(parsed.jobsdir):
+        logger.error('The path "{:s}" does not exist.'.format(parsed.jobsdir))
+        exit(1)
+    # get arguments
+    labels = set(parsed.labels.strip().strip(",").strip().split(","))
+    # read list of websites
+    with open(parsed.websites, "r") as fin:
+        websites = json.load(fin)
+    # load template jobs
+    # - Webcheck job template
+    webcheck_template_config_file = os.path.join(
+        parsed.jobsdir, WEBCHECK_TEMPLATE_JOB, "config.xml.template"
+    )
+    with open(webcheck_template_config_file, "rt") as fin:
+        webcheck_template_config = fin.read()
+    # check which configurations are valid
+    stats = {"num_jobs": 0, "num_websites": len(websites)}
+    logger.info("Found {:d} websites.".format(len(websites)))
+
+    # store things to write
+    jobs_to_write = {}
+    # ---
+    for website in websites:
+        logger.info("Analyzing [{:s}]".format(website["name"]))
+        # must have a whitelisted label
+        if len(labels.intersection(website["labels"])) <= 0:
+            continue
+        # website info
+        website_name = website["name"]
+        website_url = website["url"]
+        website_timeout = website.get("timeout", DEFAULT_TIMEOUT_MINUTES)
+
+        # create job name
+        jname = webcheck_job_name(website_name)
+        # create job by updating the template fields
+        job_config_path = os.path.join(parsed.jobsdir, jname, "config.xml")
+        params = {
+            "NAME": website_name,
+            "URL": website_url,
+            "TIMEOUT_MINUTES": website_timeout,
+        }
+        config = webcheck_template_config.format(**params)
+
+        # write job to disk
+        os.makedirs(os.path.dirname(job_config_path))
+        with open(job_config_path, "wt") as fout:
+            fout.write(config)
+        stats["num_jobs"] += 1
+
+    # print out stats
+    logger.info(
+        "Statistics: Total websites: {:d}; Total jobs: {:d}".format(stats["num_websites"], stats["num_jobs"])
+    )
+    logger.info("Done!")
+
+
+def webcheck_job_name(website_name):
+    return "Webcheck - {:s}".format(website_name)
+
+
+if __name__ == "__main__":
+    main()
